3ee22ca93918a47b2c0ee14d834d597e29228fb8
Nora Camacho <ingnorac@gmail.com>
2022-10-07 19:54:56 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.container img').forEach( image => {
+    image.onclick = () => {
+        document.querySelector('.popup').style.display = 'block';
+        document.querySelector('.popup img').src = image.getAttribute('src');
+
+    }
+} );
+
+document.querySelector('.popup span').onclick = () => {
+    document.querySelector('.popup').style.display = 'none';
+}
